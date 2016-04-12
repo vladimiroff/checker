f2f85292b57b9414269bd5ae41e9364e2c91315a
Kiril Vladimirov <kiril@vladimiroff.org>
2016-04-12 18:35:00 +0300

    Add Error type and define errors
    Error is a generic error type used inside this package. It's essentially a
    string allowing its instances to be constants.
    
    Idea of Dave Cheney: http://dave.cheney.net/2016/04/07/constant-errors
    Just testing it out.

---
+++ handlers/errors.go
@@ -0,0 +1,18 @@
+package handlers
+
+// Error is a generic error type used inside this package. It's essentially a
+// string allowing its instances to be constants.
+//
+// Idea of Dave Cheney: http://dave.cheney.net/2016/04/07/constant-errors
+type Error string
+
+func (e Error) Error() string { return string(e) }
+
+// NoSuchChecker is used when the requested implementation of
+// checks.Checker is not available. The user is expected to call the Checkers()
+// endpoint.
+const NoSuchChecker = Error("no such checker")
+
+// NoSuchCheckType is used when the Type value of a check request is not among
+// supported.
+const NoSuchCheckType = Error("no such check type")
