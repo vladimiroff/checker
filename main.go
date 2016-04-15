8942096f58b4b7c542271dd7bec1c11f67369dab
Kiril Vladimirov <kiril@vladimiroff.org>
2016-04-15 14:55:41 +0300

    Don't disable authentication on handlers

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 	k := kite.New(fmt.Sprintf("%s.checker", hostname), "1.0.0")
 	k.PreHandleFunc(handlers.LogRequest)
 	k.PostHandleFunc(handlers.LogResponse)
-	k.HandleFunc("check", handlers.LocalCheck).DisableAuthentication()
+	k.HandleFunc("check", handlers.LocalCheck)
 	k.HandleFunc("checkers", handlers.Checkers)
 
 	go k.Run()
